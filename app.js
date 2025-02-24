32008ecdaadfc8e7b6a6e55e0dcfa83bd29d3123
Max <1998max7362@mail.ru>
2025-02-25 01:37:12 +0300

    ваыва

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-// import WebSocket, { WebSocketServer } from 'ws';
-
-// const wss = new WebSocketServer({
-//   port: 8080,
-// });
-
-// wss.on('connection', onConnect);
-
-// const onConnect = (wsClient) => {
-//   console.log('Новый пользователь');
-//   // отправка приветственного сообщения клиенту
-//   wsClient.send(first_message);
-
-//   wsClient.on('message', (message) => {
-//     console.log(message);
-//   });
-
-//   wsClient.on('close', () => {
-//     // отправка уведомления в консоль
-//     console.log('Пользователь отключился');
-//   });
-// };
-
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
@@ -29,17 +6,16 @@ wss.on('connection', function connection(ws) {
   console.log('connected', ws);
   ws.send(JSON.stringify(first_message));
 
-  // while (true) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
     const parsedData = JSON.parse(data);
+    parsedData.rotators[0].maxPosition = 300;
     console.log('received: ', parsedData);
     ws.send(JSON.stringify(parsedData));
   });
 
   ws.on('close', () => console.log('closed'));
-  // }
 });
 
 const first_message = {
@@ -47,20 +23,20 @@ const first_message = {
     {
       name: 'rotator1',
       actualPosition: 10,
-      nominalPosition: 10,
-      minPosition: 10,
-      maxPosition: 10,
-      parkPosition: 100,
+      nominalPosition: 50,
+      minPosition: 0,
+      maxPosition: 100,
+      parkPosition: 150,
       goToNominal: true,
       goToPark: true,
     },
     {
       name: 'rotator2',
       actualPosition: 100,
-      nominalPosition: 100,
+      nominalPosition: 300,
       minPosition: 100,
-      maxPosition: 100,
-      parkPosition: 1000,
+      maxPosition: 200,
+      parkPosition: 200,
       goToNominal: true,
       goToPark: true,
     },
@@ -70,8 +46,8 @@ const first_message = {
       name: 'mast1',
       actualPosition: 10,
       nominalPosition: 10,
-      minPosition: 10,
-      maxPosition: 10,
+      minPosition: 200,
+      maxPosition: 300,
       parkPosition: 100,
       goToNominal: true,
       goToPark: true,
